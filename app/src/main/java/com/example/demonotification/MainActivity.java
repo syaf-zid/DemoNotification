70c74fc61c9308ea40592dadad8e993379db65e4
syafzid <18052270@myrp.edu.sg>
2020-05-29 10:56:33 +0800

    Completed with Section B.1 Worksheet

--- app/src/main/java/com/example/demonotification/MainActivity.java
+++ app/src/main/java/com/example/demonotification/MainActivity.java
@@ -8,6 +8,8 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -36,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                     NotificationChannel channel = new
                             NotificationChannel("default", "Default Channel",
-                            NotificationManager.IMPORTANCE_DEFAULT);
+                            NotificationManager.IMPORTANCE_HIGH);
 
                     channel.setDescription("This is for default notification");
                     notificationManager.createNotificationChannel(channel);
@@ -55,6 +57,12 @@ public class MainActivity extends AppCompatActivity {
                 builder.setSmallIcon(android.R.drawable.btn_star_big_off);
                 builder.setContentIntent(pIntent);
                 builder.setAutoCancel(true);
+
+                Uri uri= RingtoneManager.getDefaultUri
+                        (RingtoneManager.TYPE_NOTIFICATION);
+                builder.setSound(uri);
+
+                builder.setPriority(Notification.PRIORITY_HIGH);
 
                 Notification n = builder.build();
 
