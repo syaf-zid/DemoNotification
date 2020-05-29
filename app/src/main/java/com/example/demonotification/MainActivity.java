5c86f54533af1860509fdd383ad25a76a5a3ada3
syafzid <18052270@myrp.edu.sg>
2020-05-29 11:00:15 +0800

    Completed with Section B.2 Worksheet

--- app/src/main/java/com/example/demonotification/MainActivity.java
+++ app/src/main/java/com/example/demonotification/MainActivity.java
@@ -71,5 +71,53 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        btnNotify2 = findViewById(R.id.btnNotify2);
+
+        btnNotify2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                NotificationManager notificationManager = (NotificationManager)
+                        getSystemService(NOTIFICATION_SERVICE);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    NotificationChannel channel = new
+                            NotificationChannel("default", "Default Channel",
+                            NotificationManager.IMPORTANCE_DEFAULT);
+
+                    channel.setDescription("This is for default notification");
+                    notificationManager.createNotificationChannel(channel);
+                }
+
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                PendingIntent pIntent = PendingIntent.getActivity
+                        (MainActivity.this, requestCode,
+                                intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+                NotificationCompat.BigTextStyle bigText = new
+                        NotificationCompat.BigTextStyle();
+                bigText.setBigContentTitle("Big Text – Long Content");
+                bigText.bigText("This is one big text" +
+                        " - A quick brown fox jumps over a lazy brown dog "+
+                        "\nLorem ipsum dolor sit amet, sea eu quod des");
+                bigText.setSummaryText("Reflection Journal?");
+
+                // Build notification
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, "default");
+                builder.setContentTitle("Amazing Offer!");
+                builder.setContentText("Subject");
+                builder.setSmallIcon(android.R.drawable.btn_star_big_off);
+                builder.setContentIntent(pIntent);
+                builder.setStyle(bigText);
+                builder.setAutoCancel(true);
+
+                Notification n = builder.build();
+
+                // This replaces the existing notification with the same ID
+                notificationManager.notify(notificationID, n);
+                finish();
+            }
+        });
     }
 }
